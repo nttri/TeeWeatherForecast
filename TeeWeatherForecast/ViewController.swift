51ed19f9b2e013acebb969b757676904695fed23
nttri <bem0302@gmail.com>
2022-08-09 23:54:23 +0700

    Create project without storyboard

---
+++ TeeWeatherForecast/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TeeWeatherForecast
+//
+//  Created by tringuyen on 09/08/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        view.backgroundColor = .cyan
+    }
+
+
+}
+
